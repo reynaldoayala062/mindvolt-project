5e23121407d69ab492df62f44fdeef4af9cdef60
reynaldoayala062 <reynaldo-ayala@hotmail.com>
2021-09-03 02:32:41 -0500

    made text and service section mobile responsive

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import HeroSection from './components/HeroSection';
 import TextSection from './components/TextSection';
 import { objectOne, objectTwo } from './components/Data';
+import Services from './components/Services';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <HeroSection/>
       <TextSection {... objectOne}/>
+      <Services/>
       <TextSection {... objectTwo}/>
     </div>
   );
---
+++ src/components/Services.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import '../App.css'
+
+const Services = () => {
+    return (
+        <div id='services-container'>
+            <div id='service'>
+                <img id='service-image' src={require('../images/undraw_my_app_re_gxtj (1).svg').default} alt='' />
+                <h1>Cell phone bills</h1>
+                <p>Take care of your monthly payments online, with ease</p>
+            </div>
+            <div id='service'>
+                <img id='service-image' src={require('../images/undraw_Music_re_a2jk.svg').default} alt='' />
+                <h1>Streaming services</h1>
+                <p>Pay for subcription such as music, movies and TV streaming</p>
+            </div>
+            <div id='service'>
+                <img id='service-image' src={require('../images/undraw_sweet_home_dkhr.svg').default} alt='' />
+                <h1>Utilities</h1>
+                <p>It's simple to pay utility bills online, from water to electric</p>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Services
