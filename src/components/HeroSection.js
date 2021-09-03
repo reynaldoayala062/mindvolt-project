afa8dbe42b955240009e57e2492e83ec0d3028da
reynaldoayala062 <reynaldo-ayala@hotmail.com>
2021-09-02 19:44:34 -0500

    hero section

---
+++ src/components/HeroSection.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../App.css'
+
+const HeroSection = () => {
+    return (
+        <div id='container'>
+          <img id='rectangular' src={require('../images/pexels-andrea-piacquadio-3807763.jpg').default} alt='hero'/> 
+          <div id="hero-title">
+            <h1>Safe and smart way to pay online.</h1>
+            <h1>For just about everything.</h1>
+            <h4>Your FirstBank Visa® Debit Card.</h4>
+            <img id='debit-card' src={require('../images/personal_debit_card_firstbank2.png').default} alt=''/> 
+          </div>
+            
+        </div>
+    )
+}
+
+export default HeroSection
