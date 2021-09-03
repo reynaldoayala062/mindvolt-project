bc690b0c8840df64e2b4bb1c4dff58fe85331099
reynaldoayala062 <reynaldo-ayala@hotmail.com>
2021-09-03 18:02:06 -0500

    edit footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
             <div id='footer-logos'>
                 <img src="https://mindvolt-project.s3.us-east-2.amazonaws.com/firstbank-logo-2016.png" alt='' width='15%'/>
                 <img src='https://mindvolt-project.s3.us-east-2.amazonaws.com/Visa-Logo.png' alt='' width='10%'/>
-            </div><br/>
+            </div>
             <div id='footer-text'>
                 <p>*Visa's Zero Liability Policy does not apply to certain commercial card and anonymous prepaid card transaction or transactions not processed by Visa. Cardholders must use care in protecting their card and notify their issuing financial institution immediately of any unauthorized use. Contact your Issuer for more detail.
                 </p>
